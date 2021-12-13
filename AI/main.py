2ccd97187eed6f7af96005f186921ac0e6ce701e
Gabriel de Macedo Santos <82056067+Macedo3@users.noreply.github.com>
2021-12-13 11:22:25 -0300

    Add files via upload

---
+++ AI/main.py
@@ -0,0 +1,117 @@
+from neuralintents import GenericAssistant
+import speech_recognition
+import pyttsx3 as tts
+import sys
+
+recognizer = speech_recognition.Recognizer()
+
+speaker = tts.init()
+speaker.setProperty('rate', 150)
+
+todo_list = ['Go shopping', 'Clean Room', 'Record Video']
+
+def create_note():
+    global recognizer
+
+    speaker.say('What do you want to write into your note?')
+    speaker.runAndWait()
+
+    done = False
+    while not done:
+        try:
+            with speech_recognition.Microphone() as mic:
+                recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+                audio = recognizer.listen(mic)
+
+                note = recognizer.recognize_google(audio)
+
+                speaker.say("Choose a filename!")
+                speaker.runAndWait()
+
+                recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+                audio = recognizer.listen(mic)
+
+                filename = recognizer.recognize_google(audio)
+                filename = filename.lower()
+
+            with open(filename, 'w') as f:
+                f.write(note)
+                done = True
+                speaker.say(f'I succesfully create the note {filename}')
+                speaker.runAndWait()
+        except speech_recognition.UnknownValueError:
+            recognizer = speech_recognition.Recognizer()
+            speaker.say('I did not understand you! Please try again!')
+            speaker.runAndWait()
+
+
+def add_todo():
+    global recognizer
+
+    speaker.say("What to do you want to add?")
+    speaker.runAndWait()
+
+    done = False
+
+    while not done:
+        try:
+
+            with speech_recognition.Microphone() as mic:
+                recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+                audio = recognizer.listen(mic)
+
+                item = recognizer.recognize_google(audio)
+                item = item.lower()
+
+                todo_list.append(item)
+                done = True
+
+                speaker.say(f"I added {item} to the to do list!")
+                speaker.runAndWait()
+
+        except speech_recognition.UnknownValueError():
+            recognizer = speech_recognition.Recognizer()
+            speaker.say('I did not understand. Please try again!')
+            speaker.runAndWait()
+
+def show_todos():
+    speaker.say('The itens on to do list are the following')
+
+    for item in todo_list:
+        speaker.say(item)
+    speaker.runAndWait()
+
+def hello():
+    speaker.say("Hello. What can I do fot you?")
+    speaker.runAndWait()
+
+def quit():
+    speaker.say('Bye')
+    speaker.runAndWait()
+    sys.exit(0)
+
+mapping = {
+    "greeting": hello,
+    "create_note": create_note,
+    "add_todo": add_todo,
+    "show_todos": show_todos,
+    "exit": quit
+}
+
+assistant = GenericAssistant("intents.json")
+assistant.train_model()
+
+while True:
+
+    try:
+        with speech_recognition.Microphone() as mic:
+            recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+            audio = recognizer.listen(mic)
+
+            message = recognizer.recognize_google(audio)
+            message = message.lower()
+
+        assistant.request(message)
+
+    except speech_recognition.UnknownValueError():
+        recognizer = speech_recognition.Recognizer()
